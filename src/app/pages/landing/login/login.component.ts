409a7bc665ec7ca06bb5e4384c9015a7b9a725ab
kalan <kalanin.peto@gmail.com>
2021-09-29 19:53:15 +0200

    feat: login validation

--- src/app/pages/landing/login/login.component.ts
+++ src/app/pages/landing/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
@@ -8,8 +9,14 @@ import { AuthService } from 'src/app/services/auth.service';
 })
 export class LoginPageComponent implements OnInit {
   form: FormGroup;
+  credetialsError: boolean = false;
 
-  constructor(private authService: AuthService, private fb: FormBuilder) {
+  constructor(
+    private authService: AuthService,
+    private fb: FormBuilder,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {
     this.form = this.fb.group({
       email: [],
       password: []
@@ -21,17 +28,18 @@ export class LoginPageComponent implements OnInit {
 
   onSubmit() {
     if (this.form.valid) {
+      this.credetialsError = false;
       const { email, password } = this.form.value;
       this.authService.login(email, password)
         .subscribe(
           (value) => {
             if (value) {
-              // this.router.navigate(['../login'], { relativeTo: this.route })
+              this.router.navigate(['/acc/dashboard'])
             }
           },
           (err) => {
             console.error(err);
-
+            this.credetialsError = true;
           }
         )
     }
