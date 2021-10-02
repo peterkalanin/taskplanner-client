66207674c89fbce3c5d6b36d19216e01ff394087
kalan <kalanin.peto@gmail.com>
2021-10-02 06:15:35 +0200

    feat: caching user login token

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -3,6 +3,8 @@ import { share } from 'rxjs/operators';
 import { AuthMockService } from '../mock/auth-mock.service';
 import { User } from '../models/user.model';
 
+export const USER_STORED_KEY = 'user_stored';
+
 @Injectable({
   providedIn: 'root',
 })
@@ -13,13 +15,26 @@ export class AuthService {
     return this.userToken?.id || undefined;
   }
 
-  constructor(private authMockService: AuthMockService) {}
+  public get storedUser(): User | undefined {
+    const u = localStorage.getItem(USER_STORED_KEY);
+    return u ? JSON.parse(u) : undefined;
+  }
+  public set storedUser(value: User | undefined) {
+    localStorage.setItem(USER_STORED_KEY, JSON.stringify(value));
+  }
+
+  constructor(private authMockService: AuthMockService) {
+    if (!this.userToken) {
+      this.userToken = this.storedUser;
+    }
+  }
 
   login(email: string, password: string) {
     const obs$ = this.authMockService.login(email, password).pipe(share());
 
     obs$.subscribe((val) => {
       this.userToken = val;
+      this.storedUser = this.userToken;
     });
 
     return obs$;
