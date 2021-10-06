44fcbd3389b6ffd6b19c7f234d6a44cdf0ff56aa
kalan <kalanin.peto@gmail.com>
2021-10-06 18:02:00 +0200

    feat: none background image

--- src/app/services/theme.service.ts
+++ src/app/services/theme.service.ts
@@ -10,7 +10,7 @@ export class ThemeService {
   motives: string[] = ['wave', 'line', 'bubble', 'paws']
   motive: string = 'line';
 
-  backgrounds: string[] = ['sea', 'forest', 'westie']
+  backgrounds: string[] = ['', 'sea', 'forest', 'westie']
   background: string = 'sea';
 
   constructor() { }
