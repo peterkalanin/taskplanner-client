4f647c5ba0cff44f2995bd11cfd974cb24d61d62
kalan <kalanin.peto@gmail.com>
2021-10-08 15:57:38 +0200

    refact: task redesign

--- src/app/pages/account/tasks/tasks.component.ts
+++ src/app/pages/account/tasks/tasks.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Task } from 'src/app/models/task.model';
 import { TaskService } from 'src/app/services/task.service';
 import { ThemeService } from 'src/app/services/theme.service';
 
@@ -7,11 +8,15 @@ import { ThemeService } from 'src/app/services/theme.service';
   styleUrls: ['./tasks.component.scss']
 })
 export class TasksComponent implements OnInit {
+  tasks: Task[] = [];
 
   constructor(public theme: ThemeService, private taskService: TaskService) { }
 
   ngOnInit(): void {
-    this.taskService.getAllTasks();
+    this.taskService.getAllTasks()
+      .subscribe((response) => {
+        this.tasks = response;
+      })
   }
 
 }
